b3f3b9bfffe3314b2749f50d0c90044c324d83c0
Adriano Caversan <adriano.caversan@gmail.com>
2021-03-11 17:31:15 -0300

    - integração do js geral e da api de vagas com o layout html - ajuste em algumas nomenclaturas (js + css) para acomodar as novas divs - populando o json da api para realizar a validação das regras do js da api (localização com campos vazios ou indexistentes e grupos)

--- js/scripts/pagina.js
+++ js/scripts/pagina.js
@@ -1,5 +1,6 @@
 try {
     // Determinando o conteúdo que será carregado na página
+    //contentLoader(folder, json, type)
     contentLoader('js/data/templates/', 'header', 'header');
     contentLoader('js/data/templates/', 'video', 'video');
     contentLoader('js/data/templates/', 'time', 'gallery');
@@ -9,15 +10,16 @@ try {
 
 
     // Carregamento dos Json
-    function contentLoader(folder, content, type) {
+    function contentLoader(folder, json, type) {
         // Carregamento do json
         var loadedJson = new XMLHttpRequest();
-        loadedJson.open('GET', (folder + content + '?t=' + Math.floor(Math.random() * 1000000)), true);
+        loadedJson.open('GET', (folder + json + '?t=' + Math.floor(Math.random() * 1000000)), true);
         // Teste de integridade
         loadedJson.onreadystatechange = function() {
             if (this.readyState === 4) {
                 if (this.status >= 200 && this.status < 400) {
-                    loadContent(JSON.parse(this.responseText), content, type);
+                    console.log(folder + type)
+                    loadContent(JSON.parse(this.responseText), type);
                 } else {
                     // Error :(
                 }
@@ -27,22 +29,22 @@ try {
         // Fim do Carregamento do json
     }
 
-    function loadContent(data, content, type) {
+    function loadContent(data, type) {
         switch (type) {
             case 'header':
-                contentHeader(data, content);
+                contentHeader(data);
                 break;
             case 'video':
-                contentVideo(data, content);
+                contentVideo(data);
                 break;
             case 'gallery':
-                contentGallery(data, content);
+                contentGallery(data);
                 break;
             case 'txtgallery':
-                contentTxtGallery(data, content);
+                contentTxtGallery(data);
                 break;
             case 'vagas':
-                contentVagas(data, content);
+                contentVagas(data);
                 break;
             case 'api':
                 console.log('conteúdo vem de uma api')
@@ -53,38 +55,60 @@ try {
         }
     }
 
-    function contentHeader(data, content) {
-        document.getElementsByClassName('containerHeader')[0].getElementsByClassName('headerImage')[0].innerHTML = '<img src="img/' + data.header.imagem + '"\/>';
-        document.getElementsByClassName('containerHeader')[0].getElementsByClassName('headerTitle')[0].innerHTML += data.header.titulo;
-        document.getElementsByClassName('mainContainer')[0].getElementsByClassName('headerDescription')[0].innerHTML += data.header.descricao;
-        document.getElementsByClassName('header')[0].innerHTML += data.header.mais.link;
-        document.getElementsByClassName('header')[0].innerHTML += data.header.mais.linkname;
+    function contentHeader(data) {
+        document.getElementsByClassName('header')[0].getElementsByClassName('banner')[0].innerHTML = '<img src="img/' + data.header.imagem + '"\/>';
+        document.getElementsByClassName('header')[0].getElementsByClassName('pagetitle')[0].innerHTML += data.header.titulo;
+        document.getElementsByClassName('header')[0].getElementsByClassName('description')[0].innerHTML += data.header.descricao;
+        document.getElementsByClassName('header')[0].getElementsByClassName('more')[0].innerHTML += data.header.mais.link;
+        document.getElementsByClassName('header')[0].getElementsByClassName('more')[0].innerHTML += data.header.mais.linkname;
     }
 
-    function contentVideo(data, content) {
-        document.getElementsByClassName(content)[0].innerHTML = '<img src="img/' + data.video.src + '"\/>';
-        document.getElementsByClassName(content)[0].innerHTML += data.video.titulo;
-        document.getElementsByClassName(content)[0].innerHTML += data.video.subtitulo;
-        document.getElementsByClassName(content)[0].innerHTML += data.video.descricao;
+    function contentVideo(data) {
+        document.getElementsByClassName('videocontainer')[0].getElementsByClassName("videoplayer")[0].innerHTML = '<img src="img/' + data.video.src + '"\/>';
+        document.getElementsByClassName('videocontainer')[0].getElementsByClassName("videodescription")[0].getElementsByClassName("title")[0].innerHTML += data.video.titulo;
+        document.getElementsByClassName('videocontainer')[0].getElementsByClassName("videodescription")[0].getElementsByClassName("subtitle")[0].innerHTML += data.video.subtitulo;
+        document.getElementsByClassName('videocontainer')[0].getElementsByClassName("videodescription")[0].getElementsByClassName("description")[0].innerHTML += data.video.descricao;
     }
 
-    function contentGallery(data, content) {
-        document.getElementsByClassName(content)[0].innerHTML = data.galeria.titulo;
+    function contentGallery(data) {
+        document.getElementsByClassName('team')[0].getElementsByClassName('title')[0].innerHTML = data.galeria.titulo;
         for (i in data.galeria.imagens) {
-            document.getElementsByClassName(content)[0].innerHTML += '<img src="img/' + data.galeria.imagens[i].src + '"\/>';
+            var teampic = document.createElement('div');
+            teampic.className = 'teampic';
+            document.getElementsByClassName('team')[0].getElementsByClassName('gallery')[0].appendChild(teampic);
+            document.getElementsByClassName('team')[0].getElementsByClassName('gallery')[0].getElementsByClassName('teampic')[i].innerHTML += '<img src="img/' + data.galeria.imagens[i].src + '"\/>';
         }
     }
 
-    function contentTxtGallery(data, content) {
+    function contentTxtGallery(data) {
         for (i in data.galeriatexto) {
-            document.getElementsByClassName(content)[0].innerHTML += '<img src="img/' + data.galeriatexto[i].ico + '"\/>';
-            document.getElementsByClassName(content)[0].innerHTML += data.galeriatexto[i].titulo;
-            document.getElementsByClassName(content)[0].innerHTML += data.galeriatexto[i].descricao;
+
+            var imagecontainer = document.createElement('div');
+            imagecontainer.className = 'imagecontainer';
+            document.getElementsByClassName('aboutus')[0].getElementsByClassName('txtgallery')[0].appendChild(imagecontainer);
+
+            var picture = document.createElement('div');
+            picture.className = 'picture';
+            document.getElementsByClassName('aboutus')[0].getElementsByClassName('txtgallery')[0].getElementsByClassName('imagecontainer')[i].appendChild(picture);
+            document.getElementsByClassName('aboutus')[0].getElementsByClassName('txtgallery')[0].getElementsByClassName('imagecontainer')[i].getElementsByClassName('picture')[0].innerHTML += '<img src="img/' + data.galeriatexto[i].ico + '"\/>';
+
+            var title = document.createElement('div');
+            title.className = 'title';
+            document.getElementsByClassName('aboutus')[0].getElementsByClassName('txtgallery')[0].getElementsByClassName('imagecontainer')[i].appendChild(title);
+            document.getElementsByClassName('aboutus')[0].getElementsByClassName('txtgallery')[0].getElementsByClassName('imagecontainer')[i].getElementsByClassName('title')[0].innerHTML += data.galeriatexto[i].titulo;
+
+            var description = document.createElement('div');
+            description.className = 'description';
+            document.getElementsByClassName('aboutus')[0].getElementsByClassName('txtgallery')[0].getElementsByClassName('imagecontainer')[i].appendChild(description);
+            document.getElementsByClassName('aboutus')[0].getElementsByClassName('txtgallery')[0].getElementsByClassName('imagecontainer')[i].getElementsByClassName('description')[0].innerHTML += data.galeriatexto[i].descricao;
+
         }
     }
 
-    function contentVagas(data, content) {
-        console.log(data)
+    function contentVagas(data) {
+        document.getElementsByClassName('api_vagas')[0].getElementsByClassName('subbanner')[0].innerHTML += '<img src="img/' + data.lista.imagem + '"\/>';
+        document.getElementsByClassName('api_vagas')[0].getElementsByClassName('title')[0].innerHTML += data.lista.titulo;
+
         var apiVagas = new XMLHttpRequest();
         apiVagas.open('GET', (data.lista.listasrc + '?t=' + Math.floor(Math.random() * 1000000)), true);
         // Teste de integridade
@@ -100,26 +124,46 @@ try {
         apiVagas.send();
 
         function vagas(data) {
-            console.log(data)
             for (i in data.grupos) {
-                document.getElementsByClassName('api_vagas')[0].innerHTML += data.grupos[i].nome;
+
+                var grouplist = document.createElement('div');
+                grouplist.className = 'grouplist';
+                document.getElementsByClassName('api_vagas')[0].appendChild(grouplist);
+
+                var title = document.createElement('div');
+                title.className = 'title';
+                document.getElementsByClassName('api_vagas')[0].getElementsByClassName('grouplist')[i].appendChild(title);
+                document.getElementsByClassName('api_vagas')[0].getElementsByClassName('grouplist')[i].getElementsByClassName('title')[0].innerHTML += data.grupos[i].nome;
+
+                var list = document.createElement('div');
+                list.className = 'list';
+                document.getElementsByClassName('api_vagas')[0].getElementsByClassName('grouplist')[i].appendChild(list);
+
+                var itemCounter = 0;
                 for (j in data.grupos[i].vagas) {
-                    document.getElementsByClassName('api_vagas')[0].innerHTML += data.grupos[i].vagas[j].cargo;
-                    document.getElementsByClassName('api_vagas')[0].innerHTML += data.grupos[i].vagas[j].ativa;
-                    document.getElementsByClassName('api_vagas')[0].innerHTML += data.grupos[i].vagas[j].link;
-                    document.getElementsByClassName('api_vagas')[0].innerHTML += data.grupos[i].vagas[j].localizacao.bairro;
-                    document.getElementsByClassName('api_vagas')[0].innerHTML += data.grupos[i].vagas[j].localizacao.cidade;
-                    document.getElementsByClassName('api_vagas')[0].innerHTML += data.grupos[i].vagas[j].localizacao.pais;
+
+
+                    if (data.grupos[i].vagas[j].ativa) {
+                        var listitem = document.createElement('div');
+                        listitem.className = 'listitem';
+                        document.getElementsByClassName('api_vagas')[0].getElementsByClassName('grouplist')[i].getElementsByClassName('list')[0].appendChild(listitem);
+
+                        var position = document.createElement('position');
+                        position.className = 'position';
+                        document.getElementsByClassName('api_vagas')[0].getElementsByClassName('grouplist')[i].getElementsByClassName('list')[0].getElementsByClassName('listitem')[itemCounter].appendChild(position);
+                        document.getElementsByClassName('api_vagas')[0].getElementsByClassName('grouplist')[i].getElementsByClassName('list')[0].getElementsByClassName('listitem')[itemCounter].getElementsByClassName('position')[0].innerHTML += data.grupos[i].vagas[j].cargo;
+
+                        var location = document.createElement('location');
+                        location.className = 'location';
+                        document.getElementsByClassName('api_vagas')[0].getElementsByClassName('grouplist')[i].getElementsByClassName('list')[0].getElementsByClassName('listitem')[itemCounter].appendChild(location);
+                        document.getElementsByClassName('api_vagas')[0].getElementsByClassName('grouplist')[i].getElementsByClassName('list')[0].getElementsByClassName('listitem')[itemCounter].getElementsByClassName('location')[0].innerHTML += ((data.grupos[i].vagas[j].localizacao) ? (data.grupos[i].vagas[j].localizacao.bairro ? (data.grupos[i].vagas[j].localizacao.bairro + (data.grupos[i].vagas[j].localizacao.cidade || data.grupos[i].vagas[j].localizacao.pais ? ' - ' : '')) : '') + (data.grupos[i].vagas[j].localizacao.cidade ? (data.grupos[i].vagas[j].localizacao.cidade + (data.grupos[i].vagas[j].localizacao.pais ? ', ' : '')) : '') + (data.grupos[i].vagas[j].localizacao.pais ? (data.grupos[i].vagas[j].localizacao.pais) : '') : 'REMOTO');
+
+                        itemCounter++;
+                    }
+                    //document.getElementsByClassName('api_vagas')[0].innerHTML += data.grupos[i].vagas[j].link;
+
                 }
             }
-        }
-
-        document.getElementsByClassName(content)[0].innerHTML += '<img src="img/' + data.lista.imagem + '"\/>';
-        document.getElementsByClassName(content)[0].innerHTML += data.lista.titulo;
-        for (i in data.galeriatexto) {
-            document.getElementsByClassName(content)[0].innerHTML += '<img src="img/' + data.galeriatexto[i].ico + '"\/>';
-            document.getElementsByClassName(content)[0].innerHTML += data.galeriatexto[i].titulo;
-            document.getElementsByClassName(content)[0].innerHTML += data.galeriatexto[i].descricao;
         }
     }
 
